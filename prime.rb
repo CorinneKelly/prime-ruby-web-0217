3b11fa15a44a6130b862fca9d7990d45e3199e2b
Corinne Kelly <corinnemariekelly@gmail.com>
2017-01-31 16:13:31 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,11 @@
 # Add  code here!
+def prime?(testing_number)
+  if testing_number < 2
+    return false
+  else
+    (2...testing_number).each do |number|
+      return false if testing_number%number == 0
+    end
+  end
+ true
+end
